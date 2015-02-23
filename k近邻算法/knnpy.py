5973487afcc89d632514d23ec4c92e2969f56e7e
wuxiaojiang <717189544@qq.com>
2015-02-24 00:11:36 +0800

    holmes

--- k近邻算法/knnpy.py
+++ k近邻算法/knnpy.py
@@ -9,4 +9,15 @@ class knn():
         group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
         labels = ['A','A','B','B']
         return group,labels
+    def do(self,inx,dataset,labels,k):
+        datasetsize = dataset.shape[0]
+        #print datasetsize
+        st = tile(inx,(datasetsize,1))
+        #print st
+        diffmat = st-dataset
+        #print diffmat
+        sqdiffmat = diffmat**2
+        sqdis = sqdiffmat**0.5
+        #print sqdis
+        sort = sqdis.argsort()
         
--- k近邻算法/test.py
+++ k近邻算法/test.py
@@ -1,3 +1,4 @@
 import knnpy
 c = knnpy.knn()
-print c.createdata()
+data,lab = c.createdata()
+c.do([1.2,1.3],data,lab,7)

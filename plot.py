dc5b8671be898e45f2a3899299c38dee765efd50
wuxiaojiang <717189544@qq.com>
2015-02-23 20:46:28 +0800

    holmes

--- plot.py
+++ plot.py
@@ -1,11 +1,7 @@
-import matplotlib.pyplot as plt
-import matplotlib
 import numpy as np
-import scipy
+from pylab import*
 list1=[1.2,1.5,1.4,1.7,3.4,4.3,8.3]
 list2 = [3,5,9,8.9,4.3,6.5,1]
-fig = plt.figure()
-ax = fig.add_subplot(111)
 for i in range(len(list1)):
-    ax.scatter(list1[i],list2[i])
-plt.show()
+    scatter(list1[i],list2[i],color='#77ffcc')
+show()
